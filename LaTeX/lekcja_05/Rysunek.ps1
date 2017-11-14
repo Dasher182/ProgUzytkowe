6603d1a67df9b680fc2ef4c06437b586b39c0575
42zeroo <42zeroo@gmail.com>
2017-11-14 11:07:48 +0100

    eh

---
+++ LaTeX/lekcja_05/Rysunek.ps1
@@ -0,0 +1,67 @@
+%!PS-Adobe-3.0 EPSF-3.0
+%%BoundingBox: (atend)
+%%Title: TeXdraw drawing: Rysunek.ps1
+%%Pages: 1
+%%Creator: 1995/12/19 TeXdraw V2R0
+%%CreationDate: 2017/11/14
+50 dict begin
+/mv {stroke moveto} def
+/lv {lineto} def
+/st {currentpoint stroke moveto} def
+/sl {st setlinewidth} def
+/sd {st 0 setdash} def
+/sg {st setgray} def
+/bs {gsave} def /es {stroke grestore} def
+/fl {gsave setgray fill grestore
+ currentpoint newpath moveto} def
+/fp {gsave setgray fill grestore st} def
+/cv {curveto} def
+/cr {gsave currentpoint newpath 3 -1 roll 0 360 arc
+ stroke grestore} def
+/fc {gsave setgray currentpoint newpath
+ 3 -1 roll 0 360 arc fill grestore} def
+/ar {gsave currentpoint newpath 5 2 roll arc stroke grestore} def
+/el {gsave /svm matrix currentmatrix def
+ currentpoint translate scale newpath 0 0 1 0 360 arc
+ svm setmatrix stroke grestore} def
+/fe {gsave setgray currentpoint translate scale newpath
+ 0 0 1 0 360 arc fill grestore} def
+/av {/hhwid exch 2 div def /hlen exch def
+ /ah exch def /tipy exch def /tipx exch def
+ currentpoint /taily exch def /tailx exch def
+ /dx tipx tailx sub def /dy tipy taily sub def
+ /alen dx dx mul dy dy mul add sqrt def
+ /blen alen hlen sub def
+ gsave tailx taily translate dy dx atan rotate
+ (V) ah ne {blen 0 gt {blen 0 lineto} if} {alen 0 lineto} ifelse
+ stroke blen hhwid neg moveto alen 0 lineto blen hhwid lineto
+ (T) ah eq {closepath} if
+ (W) ah eq {gsave 1 setgray fill grestore closepath} if
+ (F) ah eq {fill} {stroke} ifelse
+ grestore tipx tipy moveto} def
+0.24 0.24 scale
+1 setlinecap 1 setlinejoin
+3 setlinewidth [] 0 setdash
+0 0 moveto
+4 sl
+0 0 mv
+9 0 fc
+591 118 mv
+9 0 fc
+118 354 mv
+9 0 fc
+0 0 mv
+118 354 lv
+591 118 lv
+0 0 lv
+0 0 mv
+118 cr
+591 118 mv
+118 cr
+118 354 mv
+118 0.5 fc
+0 0 0 0 591 118 cv
+stroke end showpage
+%%Trailer:
+%%BoundingBox: -28 -28 170 113
+%%EOF
